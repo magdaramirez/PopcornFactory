c37774e5f38580c59c08e8c1b255f2592c089fcc
LAPTOP-SL2F8D5S\magda <magda-rmzz@hotmail.com>
2024-03-07 16:52:32 -0700

    commit practica 7

---
+++ app/src/main/java/mx/edu/potros/practica6/Cliente.kt
@@ -0,0 +1,6 @@
+package mx.edu.potros.practica6
+
+data class Cliente (var nombre:String,
+                    var tipoPago:String,
+                    var asiento:Int) {
+}
--- app/src/main/java/mx/edu/potros/practica6/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/potros/practica6/detalle_pelicula.kt
@@ -1,11 +1,15 @@
 package mx.edu.potros.practica6
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
 class detalle_pelicula : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_pelicula)
@@ -13,12 +17,18 @@ class detalle_pelicula : AppCompatActivity() {
         val ivPeliculaImage: ImageView = findViewById(R.id.ivPeliculaImagen)
         val tvNombrePelicula: TextView = findViewById(R.id.tvNombrePelicula)
         val tvPeliculaDesc: TextView = findViewById(R.id.tvPeliculaDesc)
+        val btnBuyTickets: Button = (findViewById(R.id.btnBuyTickets))
 
         val bundle=intent.extras
         if(bundle != null){
             ivPeliculaImage.setImageResource(bundle.getInt("header"))
             tvNombrePelicula.setText(bundle.getString("titulo"))
             tvPeliculaDesc.setText(bundle.getString("sinopsis"))
+        }
+
+        btnBuyTickets.setOnClickListener{
+                var intent: Intent = Intent(this, seatSelection::class.java)
+                this.startActivity(intent)
         }
     }
 }
